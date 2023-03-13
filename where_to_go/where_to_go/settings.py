75a01b58c292510a0d89c244b42d7db6e076d1bd
mv-31 <96203101+mv-31@users.noreply.github.com>
2023-03-13 17:04:36 +0300

    Add frontend

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -65,6 +65,12 @@ TEMPLATES = [
     },
 ]
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 WSGI_APPLICATION = 'where_to_go.wsgi.application'
 
 # Database
--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
