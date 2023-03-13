01e9d6a2240152f2f5521af6c2ed786a91cde11d
mv-31 <96203101+mv-31@users.noreply.github.com>
2023-03-14 00:16:36 +0300

    Move data from template to view

--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from geojson import Feature, FeatureCollection, Point
 
-# Create your views here.
+from .models import Place
+
+
+class PlaceListView(ListView):
+    model = Place
+    template_name = 'index.html'
+    context_object_name = 'places'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        places = self.get_queryset()
+        features = []
+        for place in places:
+            feature = Feature(
+                geometry=Point((place.lng, place.lat)),
+                properties={
+                    'title': place.title,
+                    'placeId': '',
+                    'detailsUrl': ''
+                }
+            )
+            features.append(feature)
+        geojson_data = FeatureCollection(features)
+        context['geojson_data'] = geojson_data
+        return context
--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static
-from django.urls import path
-from django.views.generic import TemplateView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', include('places.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
