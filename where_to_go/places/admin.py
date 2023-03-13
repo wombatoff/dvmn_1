997120653d6039c425f2a466220f089a4c3fd245
mv-31 <96203101+mv-31@users.noreply.github.com>
2023-03-13 19:29:13 +0300

    Add Place model

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Place
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description_short',
+        'description_long',
+        'lng',
+        'lat',
+    )
+
+    empty_value_display = '-пусто-'
+
--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.TextField()
+    description_long = models.TextField()
+    lng = models.FloatField()
+    lat = models.FloatField()
+
+    def __str__(self):
+        return self.title
