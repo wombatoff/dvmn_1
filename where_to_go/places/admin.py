dc3d88a81d13157532201302552e00b6c2637d95
mv-31 <96203101+mv-31@users.noreply.github.com>
2023-03-14 21:01:10 +0300

    add sorting image in admin

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,20 +1,26 @@
+from adminsortable2.admin import SortableAdminMixin, SortableAdminBase, SortableStackedInline
 from django.contrib import admin
 from django.utils.html import format_html
 
 from .models import Place, Image
 
 
-class ImageInLine(admin.TabularInline):
+class ImageInLine(SortableStackedInline):
     model = Image
-    list_display = ('id', 'place', 'image_preview')
     readonly_fields = ('image_preview',)
+    ordering = ('order',)
+    list_display = (
+        'order',
+        'place',
+        'image_preview',
+    )
 
     def image_preview(self, obj):
         return format_html(f'<img src="{obj.image.url}" width="200"/>')
 
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     list_display = (
         'title',
         'placeId',
@@ -28,11 +34,15 @@ class PlaceAdmin(admin.ModelAdmin):
 
 
 @admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
+class ImageAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = (
         'place',
+        'image_preview',
         'order',
         'image',
     )
-
     empty_value_display = '-пусто-'
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        return format_html(f'<img src="{obj.image.url}" width="200"/>')
--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -20,7 +20,11 @@ class Image(models.Model):
         related_name='images'
     )
     image = models.ImageField()
-    order = models.IntegerField()
+    order = models.PositiveIntegerField(
+        default=0,
+        blank=False,
+        null=False,
+    )
 
     def __str__(self):
         return self.image.name
