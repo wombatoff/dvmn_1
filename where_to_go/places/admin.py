fc0efb695321f028bf816102c22322d9a564434c
mv-31 <96203101+mv-31@users.noreply.github.com>
2023-03-14 19:58:52 +0300

    add image preview in admin

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,22 +1,29 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Place, Image
 
 
 class ImageInLine(admin.TabularInline):
     model = Image
+    list_display = ('id', 'place', 'image_preview')
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        return format_html(f'<img src="{obj.image.url}" width="200"/>')
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = (
         'title',
+        'placeId',
         'description_short',
         'description_long',
         'lng',
         'lat',
     )
-    inlines = [ImageInLine,]
+    inlines = [ImageInLine, ]
     empty_value_display = '-пусто-'
 
 
@@ -29,6 +36,3 @@ class ImageAdmin(admin.ModelAdmin):
     )
 
     empty_value_display = '-пусто-'
-
-
-
