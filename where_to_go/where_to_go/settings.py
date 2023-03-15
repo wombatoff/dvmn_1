58464bf88dc147827142137a335e642b22700c66
mv-31 <96203101+mv-31@users.noreply.github.com>
2023-03-15 12:35:24 +0300

    set SECRET_KEY

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -76,9 +76,7 @@ TEMPLATES = [
 ]
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
