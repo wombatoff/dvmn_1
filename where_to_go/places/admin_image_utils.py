5370ffb33485458b8f2927343c62447280df2156
mv-31 <96203101+mv-31@users.noreply.github.com>
2023-03-20 12:09:50 +0300

    Refactor: minor fix admin_image_utils (image_preview)

--- where_to_go/places/admin_image_utils.py
+++ where_to_go/places/admin_image_utils.py
@@ -1,5 +1,5 @@
 from django.utils.html import format_html
 
 
-def image_preview(obj):
-    return format_html('<img src="{}" height ="200"/>', obj.image.url)
+def image_preview(place_image):
+    return format_html('<img src="{}" height ="200"/>', place_image.image.url)
