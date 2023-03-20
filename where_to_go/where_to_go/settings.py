e1e88ede90edc84434b40f2fc82e31bbe5af251a
mv-31 <96203101+mv-31@users.noreply.github.com>
2023-03-20 10:50:49 +0300

    Refactor: minor fix settings.py (DEBUG)

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DJANGO_', default=False)
+DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default='localhost')
 
